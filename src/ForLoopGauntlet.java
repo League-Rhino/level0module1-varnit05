93bd206234ad2a6289f3f4b452dcfd4fd997e084
varnit05 <league@WorthyMini-6.attlocal.net>
2017-05-22 19:53:56 -0700

    5/22/17
    did for loop gauntlet .

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,172 @@
+import java.time.chrono.MinguoChronology;
+
+import javax.swing.JOptionPane;
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+
+	// gauntlet 2
+//	for (int i = 100; i > -1; i--) {
+//System.out.println(i);	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
+
+//	for (int x = 0; x < 101; x++) {
+//		System.out.println(x);	
+//
+//			
+//			
+//		
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			}
+
+
+
+
+
+
+
+//
+//
+//
+//	for (int l = 2; l < 101; l+=2) {
+//
+//			System.out.println(l);	
+//
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+//			}
+
+
+
+
+
+
+
+//
+//	for (int o = 1; o < 100; o+=2) {
+//		
+//			System.out.println(o);	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//}	
+//		
+
+
+//	for (int m = 0; m < 501; m++) {
+//		if((m % 2)==0){System.out.println(m+ " even");}
+//
+//		else{System.out.println(m+ " odd");
+//
+//
+//
+//	
+//}
+//
+
+
+//
+//	}
+//
+//
+//
+//
+//
+//
+//}
+//
+
+
+
+
+
+
+//
+//}
+
+	for (int f = 0; f < 12; f++) {
+		
+	}
+	
+System.out.println("In 2005, I was 0 years old")	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
